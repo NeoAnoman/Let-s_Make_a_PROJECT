fd7a26167ea8de9e6649f4f8c99c7bb3c1cfabed
DarkStarp <47856395+DarkStarp@users.noreply.github.com>
2019-04-15 22:21:03 -0700

    udated sats.c

--- main_project/stat.c
+++ main_project/stat.c
@@ -3,7 +3,17 @@
     char name[20];
 };*/
 void rmstat(struct user u)
-{
+{struct statistics
+{int ID;
+ struct tscore t;
+ struct fscore f;
+ int wscore;
+}sts;
+ f1=FILE*rms=fopen("score.txt","r");
+ while(!feof(f1))
+ {fread(sts,size of(sts),1,f);
+  if(sts.id==u.id)
+  {printf("TIME ATTACK"
 
 }
 void chstat(struct user u)
