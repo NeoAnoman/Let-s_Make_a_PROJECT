ea8214da9fb6a82e82b6c127e15829855d8831fd
Neo Anoman <42651314+NeoAnoman@users.noreply.github.com>
2019-04-16 14:19:39 +0530

    Added Exit menu in typingmaster.c

--- main_project/Typing_Master.c
+++ main_project/Typing_Master.c
@@ -1,11 +1,24 @@
 #include<stdio.h>
 #include "user.c"
+#include "Project.c"
 int welcome()
 {
+    char cr[30];
+    SetColor(99);
+    gotoxy(0,5);
+    FILE *fil=fopen("typing.txt","r");
+    while(!feof(fil))
+    {
+        fgets(cr,30,fil);
+        printf("%s",cr);
+    }
+    delay(2);
+    SetColor(98);
+    system("cls");
     printf("*******Welcome to Typing Master******\nUse arrow keys to navigate and 'enter' to make a selection\n\n");
     int i=1;
     char c;
-    char s1[]="1.****New User****\n2.Existing User",s2[]="1.New User\n2.****Existing User****";
+    char s1[]="1.****New User****\n2.Existing User\n3.Exit",s2[]="1.New User\n2.****Existing User****\n3.Exit",s3[]="1.New User\n2.Existing User\n****3.Exit****";
     puts(s1);
     while(i!=0)
     {
@@ -17,9 +30,10 @@ int welcome()
         case 13:
             if(i==1)
                 newuser();
-            else
+            else if(i==2)
                 user();
-            i=0;
+            else if(i==3)
+                i=0;
             break;
         case 72:
             if(i==2)
@@ -29,12 +43,19 @@ int welcome()
                 puts(s1);
                 i=1;
             }
-            else
+            else if(i==3)
             {
                 system("cls");
                 printf("*******Welcome to Typing Master******\nUse arrow keys to navigate and 'enter' to make a selection\n\n");
                 puts(s2);
                 i=2;
+            }
+            else if(i==1)
+            {
+                system("cls");
+                printf("*******Welcome to Typing Master******\nUse arrow keys to navigate and 'enter' to make a selection\n\n");
+                puts(s3);
+                i=3;
             }
             break;
         case 80:
@@ -42,15 +63,22 @@ int welcome()
             {
                 system("cls");
                 printf("*******Welcome to Typing Master******\nUse arrow keys to navigate and 'enter' to make a selection\n\n");
-                puts(s1);
-                i=1;
+                puts(s3);
+                i=3;
             }
-            else
+            else if(i==1)
             {
                 system("cls");
                 printf("*******Welcome to Typing Master******\nUse arrow keys to navigate and 'enter' to make a selection\n\n");
                 puts(s2);
                 i=2;
+            }
+            else if(i==3)
+            {
+                system("cls");
+                printf("*******Welcome to Typing Master******\nUse arrow keys to navigate and 'enter' to make a selection\n\n");
+                puts(s1);
+                i=1;
             }
         }
     }
